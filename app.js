7411e26e7e449495ca57a05775088fa5f0ad08c4
ritsaha <145288435+ritsaha@users.noreply.github.com>
2026-08-16 20:32:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ADMIN_PIN = "140909";                // CHANGE this PIN
 
 const PRODUCTS = [
   {id:1,name:"Cream Linen Trousers",price:699,size:"M",condition:"Excellent",image:"file_000000001a40820bb31806519beb2fb9.png"},
-  {id:2,name:"Black Double-Breasted Overcoat",price:1499,size:"M",condition:"Excellent",image:"file_00000000da8481fa86f2f6b7e9e4d300.png"},
+  {id:2,name:"Black Double-Breasted Overcoat",price:2000,size:"M",condition:"Excellent",image:"file_00000000da8481fa86f2f6b7e9e4d300.png"},
   {id:3,name:"premium brown leather jacket",price:1199,size:"M",condition:"Very good",image:"file_000000003c40820bbbdc94ccaa729a0f.png"}
 ];
 
